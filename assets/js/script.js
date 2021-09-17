51f62b591f9cc06e0e38093e061d7c1889195330
Lia OBrien <liaobrien123@gmail.com>
2021-09-17 15:26:22 -0400

    added functionality that changes colors of textbox depending on hour of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
-
-
-
 var currentDay = moment().format("dddd MMMM Do");
 $("#currentDay").text(currentDay);
 
+
+// call current hour of the day
 var currentHour = moment().format("H");
 console.log(currentHour);
 
+// coverted the string value of currentHour into a number that can be compared to
+var getCurrentHourInt = parseInt(currentHour);
+console.log(getCurrentHourInt);
+
+
+for (let i = 9; i <= 17; i++) {
+      if (i < getCurrentHourInt) {
+            $("#" + i).addClass("past");
+      }
+      else if (i === getCurrentHourInt) {
+            $("#" + i).addClass("present");
+      }
+      else if (i > getCurrentHourInt) {
+            $("#" + i).addClass("future");
+      }
+}
 
 
 
@@ -20,6 +35,8 @@ console.log(currentHour);
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // need to compare current hour to the hour somehow designated in textarea?
+// if currenthour > timeblockhour, then textarea addclass past
+// how do i make the textareas each have their own hour? ids? dataset?
 
 // WHEN I click into a timeblock
 // THEN I can enter an event
